2ae396b30ce9fb440a894d9d5d5439094c63f788
Armando Ortiz <armando@solairo.co.jp>
2019-12-03 18:55:44 +0900

    ignition and subscription not required

--- muv.ts
+++ muv.ts
@@ -13,8 +13,8 @@ export type MUV<M,U,V,I,S,A,E> = {
   model: M,
   update: (m: M) => (a: A) => { model: M, effects: E[] },
   view: (d: (a: A) => void) => (m: M) => View,
-  ignition: (d: (a: A) => void) => void,
-  subscriptions: (d: (a: A) => void) => (e: E) => void
+  ignition?: (d: (a: A) => void) => void,
+  subscriptions?: (d: (a: A) => void) => (e: E) => void
 };
 
 export function muv<M,U,V,I,S,A,E>(muv: MUV<M,U,V,I,S,A,E>){
@@ -39,7 +39,7 @@ export function muv<M,U,V,I,S,A,E>(muv: MUV<M,U,V,I,S,A,E>){
     oldView = view(dispatch)(model);
     root.appendChild(oldView.render());
 
-    subs = subscriptions(dispatch);
-    ignition(dispatch)
+    if(subscriptions) subs = subscriptions(dispatch);
+    if(ignition) ignition(dispatch)
   };
 }
